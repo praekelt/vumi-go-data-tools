6a9eed35507b50e924655b8a1350240ac79953ac
Simon de Haan <simon@praekeltfoundation.org>
2014-03-06 11:02:57 +0200

    filters + pipeline & tests

--- gdt/tests/test_gdt.py
+++ gdt/tests/test_gdt.py
@@ -2,7 +2,9 @@ from StringIO import StringIO
 from unittest import TestCase
 import csv
 
-from gdt.vumigomessage import VumiGoMessageParser
+from gdt.vumigomessage import (
+    VumiGoMessageParser, DirectionalFilter, MSISDNFilter, TimestampFilter,
+    FilterPipeline, FilterException)
 
 
 class GdtTestCase(TestCase):
@@ -83,3 +85,85 @@ class RefactoredGdtTestCase(TestCase):
                 ",,,,default\r\n")
         output = self.parse(parser, line)
         self.assertEqual(output, '')
+
+
+class FilterTestCase(TestCase):
+
+    def test_directional_filter(self):
+        f = DirectionalFilter('inbound')
+        self.assertTrue(f.apply({'direction': 'inbound'}))
+        self.assertFalse(f.apply({'direction': 'outbound'}))
+        self.assertFalse(f.apply({}))
+
+    def test_msisdn_filter(self):
+        f = MSISDNFilter('to_addr', '123')
+        self.assertTrue(f.apply({'to_addr': '123'}))
+        self.assertFalse(f.apply({'from_addr': '123'}))
+
+        f = MSISDNFilter('from_addr', '123')
+        self.assertTrue(f.apply({'from_addr': '123'}))
+        self.assertFalse(f.apply({'to_addr': '123'}))
+
+    def test_timestamp_filter(self):
+        f = TimestampFilter('2013-01-01')
+        self.assertTrue(f.apply({'timestamp': '2013-01-01'}))
+        self.assertFalse(f.apply({'timestamp': '2012-01-01'}))
+
+        f = TimestampFilter('2013-01-01', '2013-02-01')
+        self.assertTrue(f.apply({'timestamp': '2013-01-01'}))
+        self.assertFalse(f.apply({'timestamp': '2013-03-01'}))
+
+        self.assertRaises(
+            FilterException, TimestampFilter,
+            '2013-01-01', '2000-01-01')
+
+    def test_filter_chaining(self):
+        f = TimestampFilter('2013-01-01').chain(
+            DirectionalFilter('inbound'))
+        self.assertTrue(f.process({
+            'direction': 'inbound', 'timestamp': '2013-01-01'}))
+        self.assertFalse(f.process({
+            'direction': 'outbound', 'timestamp': '2013-01-01'}))
+        self.assertFalse(f.process({
+            'direction': 'inbound', 'timestamp': '2000-01-01'}))
+
+        f = TimestampFilter('2013-01-01', '2013-12-31').chain(
+            DirectionalFilter('inbound'))
+
+        self.assertTrue(f.process({
+            'direction': 'inbound', 'timestamp': '2013-06-01'}))
+        self.assertFalse(f.process({
+            'direction': 'inbound', 'timestamp': '2014-06-01'}))
+
+
+class FilterPipelineTestCase(TestCase):
+
+    HEADER = ("timestamp,from_addr,to_addr,content,message_id,in_reply_to,"
+              "session_event,transport_type,direction,"
+              "network_handover_status,network_handover_reason,"
+              "delivery_status,endpoint\r\n")
+    INBOUND = ("2013-09-10 19:24:03.289543,+27817030792,*120*8864*1203#,"
+               ",af266289e40949388b5a8cacb4a2d13a,,new,ussd,inbound"
+               ",,,,default\r\n")
+    OUTBOUND = ("2013-09-11 19:24:03.289543,+27817030792,27123456789,"
+                ",af266289e40949388b5a8cacb4a2d13b,,resume,ussd,outbound"
+                ",,,,default\r\n")
+    SAMPLE = HEADER + INBOUND + OUTBOUND
+
+    def test_filter_pipeline(self):
+        fp = FilterPipeline([DirectionalFilter('inbound')])
+        stdin = StringIO(self.SAMPLE)
+        stdout = StringIO()
+        fp.process(stdin=stdin, stdout=stdout)
+        self.assertEqual(stdout.getvalue(), self.HEADER + self.INBOUND)
+
+    def test_filter_pipeline_chaining(self):
+        fp = FilterPipeline([
+            DirectionalFilter('inbound').chain(
+                MSISDNFilter('from_addr', '+27817030792')),
+            TimestampFilter('2013-09-10 00:00:00', '2013-09-10 23:59:59')
+        ])
+        stdin = StringIO(self.SAMPLE)
+        stdout = StringIO()
+        fp.process(stdin=stdin, stdout=stdout)
+        self.assertEqual(stdout.getvalue(), self.HEADER + self.INBOUND)
--- gdt/vumigomessage.py
+++ gdt/vumigomessage.py
@@ -4,6 +4,85 @@ import dateutil.parser
 import sys
 
 
+class FilterException(Exception):
+    pass
+
+
+class Filter(object):
+
+    def __init__(self):
+        self._chain = []
+
+    def apply(self, row):
+        raise NotImplemented('Subclasses should implement')
+
+    def chain(self, filter):
+        self._chain.append(filter)
+        return self
+
+    def process(self, row):
+        return reduce(
+            lambda accumulator, filter_: accumulator and filter_.apply(row),
+            self._chain, self.apply(row))
+
+
+class DirectionalFilter(Filter):
+
+    def __init__(self, direction):
+        super(DirectionalFilter, self).__init__()
+        self.direction = direction
+
+    def apply(self, row):
+        return row.get('direction') == self.direction
+
+
+class MSISDNFilter(Filter):
+
+    def __init__(self, addr_direction, msisdn):
+        super(MSISDNFilter, self).__init__()
+        if addr_direction not in ['to_addr', 'from_addr']:
+            raise FilterException
+        self.addr_direction = addr_direction
+        self.msisdn = msisdn
+
+    def apply(self, row):
+        return row.get(self.addr_direction) == self.msisdn
+
+
+class TimestampFilter(Filter):
+
+    def __init__(self, start, end=None):
+        super(TimestampFilter, self).__init__()
+        self.start = dateutil.parser.parse(start)
+        self.end = (dateutil.parser.parse(end) if end is not None else None)
+        if self.end and self.end < self.start:
+            raise FilterException(
+                'End timestamp must come after start timestamp.')
+
+    def apply(self, row):
+        vumitimestamp = dateutil.parser.parse(row['timestamp'])
+        if self.end is not None:
+            return self.start <= vumitimestamp < self.end
+        return self.start <= vumitimestamp
+
+
+class FilterPipeline(object):
+
+    def __init__(self, filters=None):
+        self.filters = ([] if filters is None else filters)
+        self._chain = []
+
+    def process(self, stdin=sys.stdin, stdout=sys.stdout):
+        reader = csv.DictReader(stdin)
+        writer = csv.DictWriter(stdout, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        for row in reader:
+            for filter_ in self.filters:
+                if filter_.process(row):
+                    writer.writerow(row)
+                    break
+
+
 class VumiGoMessageParser(object):
 
     stdin = sys.stdin
@@ -81,10 +160,19 @@ class VumiGoMessageParser(object):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Vumi Go Messages CSV Parser')
-    parser.add_argument('-m','--msisdn', help='MSISDN to extract messages', required=False)
-    parser.add_argument('-d','--direction', help='Message direction to extract', required=False, choices=['inbound', 'outbound', 'all'])
-    parser.add_argument('-s','--start', help='Date time to start from (as ISO timestamp, e.g. 2013-09-01 01:00:00)', required=False)
-    parser.add_argument('-e','--end', help='Date time to extract to (as ISO timestamp, e.g. 2013-09-10 03:00:00)', required=False)
+    parser.add_argument(
+        '-m', '--msisdn', help='MSISDN to extract messages', required=False)
+    parser.add_argument(
+        '-d', '--direction', help='Message direction to extract',
+        required=False, choices=['inbound', 'outbound', 'all'])
+    parser.add_argument(
+        '-s', '--start',
+        help=('Date time to start from '
+              '(as ISO timestamp, e.g. 2013-09-01 01:00:00)'), required=False)
+    parser.add_argument(
+        '-e', '--end',
+        help=('Date time to extract to '
+              '(as ISO timestamp, e.g. 2013-09-10 03:00:00)'), required=False)
 
     args = parser.parse_args()
     gdt = VumiGoMessageParser(vars(args))
