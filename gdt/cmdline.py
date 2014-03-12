3967e246b61662816c7bf036b0470f9a475a04a4
Simon de Haan <simon@praekeltfoundation.org>
2014-03-12 17:42:20 +0200

    structuring cmdline

--- gdt/cmdline.py
+++ gdt/cmdline.py
@@ -5,20 +5,36 @@ import dateutil.parser
 def get_parser():
 
     parser = argparse.ArgumentParser(description='Vumi Go Data Tools')
-    parser.add_argument(
-        '-m', '--msisdn', help='MSISDN to extract messages', required=False)
-    parser.add_argument(
-        '-d', '--direction', help='Message direction to extract',
-        required=False, choices=['inbound', 'outbound', 'all'])
-    parser.add_argument(
+
+    subparsers = parser.add_subparsers(help='use `command --help`.')
+
+    msisdn_parser = subparsers.add_parser(
+        'msisdn', help='Filter on an msisdn')
+    msisdn_parser.add_argument(
+        '-m', '--msisdn', help='MSISDN to extract messages for',
+        required=True)
+    msisdn_parser.add_argument(
+        '-t', '--addr-type',
+        help='Which address type to filter on.',
+        choices=['to_addr', 'from_addr'], required=True)
+
+    daterange_parser = subparsers.add_parser(
+        'daterange', help='Filter on a date range.')
+    daterange_parser.add_argument(
         '-s', '--start',
         help=('Date time to start from '
               '(as ISO timestamp, e.g. 2013-09-01 01:00:00)'),
         required=False, type=dateutil.parser.parse)
-    parser.add_argument(
+    daterange_parser.add_argument(
         '-e', '--end',
         help=('Date time to extract to '
               '(as ISO timestamp, e.g. 2013-09-10 03:00:00)'),
         required=False, type=dateutil.parser.parse)
+
+    direction_parser = subparsers.add_parser(
+        'direction', help='Filter on message direction.')
+    direction_parser.add_argument(
+        '-d', '--direction', help='Message direction to extract',
+        required=False, choices=['inbound', 'outbound'])
 
     return parser
