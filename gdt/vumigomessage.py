772595c551f70f9339bc9bef054a1a15e8074aa7
Mike Jones <mike@westerncapelabs.com>
2014-03-12 11:30:13 +0200

    Rebuilt run function

--- gdt/vumigomessage.py
+++ gdt/vumigomessage.py
@@ -91,74 +91,30 @@ class VumiGoMessageParser(object):
     stdin = sys.stdin
     stdout = sys.stdout
 
-    errors = []
-
-    header = ["timestamp", "from_addr", "to_addr", "content",
-                "message_id", "in_reply_to", "session_event", "transport_type",
-                "direction", "network_handover_status", "network_handover_reason",
-                "delivery_status", "endpoint"]
-
-    # Header positions
-    timestamp = 0
-    from_addr = 1
-    to_addr = 2
-    content = 3
-    message_id = 4
-    in_reply_to = 5
-    session_event = 6
-    transport_type = 7
-    direction = 8
-    network_handover_status = 9
-    network_handover_reason = 10
-    delivery_status = 11
-    endpoint = 12
 
     def __init__(self, args):
         self.args = args
 
-    def filtered(self, message):
-        # check message and write out if it matches filter
-        matches = False
-        if 'msisdn' in self.args:
-            matches = False # reset because if defined, needs to match
-            if self.args['direction'] == "all" and self.args['msisdn'] in [message[self.from_addr], message[self.to_addr]]:
-                matches = True
-            elif self.args['direction'] == "inbound" and self.args['msisdn'] == message[self.from_addr]:
-                matches = True
-            elif self.args['direction'] == "outbound" and self.args['msisdn'] == message[self.to_addr]:
-                matches = True
-        if 'start' in self.args and 'end' in self.args:
-            matches = False # reset because if defined, needs to match
-            vumitimestamp = dateutil.parser.parse(message[self.timestamp])
-            start = dateutil.parser.parse(self.args['start'])
-            end = dateutil.parser.parse(self.args['end'])
-            if (vumitimestamp > start and vumitimestamp < end):
-                matches = True
-        # output
-        if matches:
-            csv.writer(self.stdout).writerow(message)
-
-    def extracted(self, message):
-        # check message and collect metrics
-        pass
 
     def run(self):
-        line = 0
-        for message in csv.reader(self.stdin, delimiter=',', quotechar='"'):
-            line += 1
-            self.handle_message(message)
-        if len(self.errors) != 0:
-            self.stdout.write(unicode(self.errors))
+        filters = []
+        if 'msisdn' in self.args:
+            filters.append(MSISDNFilter('from_addr', self.args['msisdn']))
+            filters.append(MSISDNFilter('to_addr', self.args['msisdn']))
+        if 'direction' in self.args:
+            if self.args['direction'] == "all":
+                filters.append(DirectionalFilter('inbound'))
+                filters.append(DirectionalFilter('outbound'))
+            else:
+                filters.append(DirectionalFilter(self.args['direction']))
+        if 'start' in self.args:
+            if 'end' not in self.args:
+                self.args['end'] = None
+            filters.append(TimestampFilter(self.args['start'], self.args['end']))
 
-    def handle_message(self, message):
-        if message == self.header:
-            pass
-        elif len(message) == 13:
-            # Start processing
-            self.filtered(message)
-            self.extracted(message)
-        else:
-            self.errors.append([message, "Unparsable entry"])
+        fp = FilterPipeline(filters)
+        fp.process(stdin=self.stdin, stdout=self.stdout)
+ 
 
 if __name__ == '__main__':
 
