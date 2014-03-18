057ea40098caa52e043aa6f6b2f7cb3577498f97
Mike Jones <mike@westerncapelabs.com>
2014-03-18 11:16:03 +0200

    Hold clarification on week 1 status

--- gdt/cmdline.py
+++ gdt/cmdline.py
@@ -99,7 +99,7 @@ def get_parser():
         '-y', '--year', help='The year to extract week(s) from',
         dest='year', type=int, required=False)
     weekrange_parser.add_argument(
-        '-w', '--weeks', help='The week(s) to extract',
+        '-w', '--weeks', help='The week(s) to extract. Note: Week 1 is the week with Jan 4th in it.',
         dest='weeks', type=int, required=False, nargs='+')
     weekrange_parser.set_defaults(subcommand_name='weekrange')
 
